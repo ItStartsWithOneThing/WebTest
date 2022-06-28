7f1fcb856cfa64ce4e74ca693f4c69a9e543deaf
Konstantin Turkin <like69.kt@gmail.com>
2022-06-28 22:23:24 +0300

    fixup! added multi-layered architecture, automapper, DI

---
+++ WebTestDAL/IBodyKitRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using WebTestDAL.Entities;
+
+namespace WebTestDAL
+{
+    public interface IBodyKitRepository
+    {
+        Guid Add(BodyKitDto kit);
+        IEnumerable<BodyKitDto> GetAll();
+        BodyKitDto GetById(Guid id);
+        BodyKitDto GetBySeveralParams(Guid id, string rear); // Existing for instance
+        bool Remove(Guid id);
+        bool Update(BodyKitDto newKit);
+        BodyKitDto UpdateRearBumper(Guid id, string newRearBumper);
+    }
+}
