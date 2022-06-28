29d749f8460ba6ca36517f61f45106a49525a497
Konstantin Turkin <like69.kt@gmail.com>
2022-06-28 22:33:48 +0300

    fixup! fixup! fixup! added multi-layered architecture, automapper, DI

--- WebTestBL/BodyKitService.cs
+++ WebTestBL/BodyKitService.cs
@@ -59,7 +59,7 @@ namespace WebTestBL
             return result;
         }
 
-        public BodyKit GetByQuerryString(Guid id, string rear)
+        public BodyKit GetByQuerryString(Guid id, string rear) // Existing for instance
         {
             var dbBodyKit = _repository.GetBySeveralParams(id, rear);
 
